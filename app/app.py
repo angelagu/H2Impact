48098f3375d6ca4b4cedc4a4449f14c004887b50
Alice Rao <arao@arao.local>
2015-06-27 13:23:19 -0700

    Bootstrap template updates

--- app/app.py
+++ app/app.py
@@ -5,12 +5,21 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('intro.html')
 
 
-@app.route('/hello')
-def hello():
-    return render_template('hello.html')
+@app.route('/account')
+def account():
+    return render_template('bootstrap_index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/mysage')
+def my_sage():
+    return render_template('my_sage.html')
 
 
 if __name__ == '__main__':
