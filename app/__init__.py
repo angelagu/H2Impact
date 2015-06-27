b6a043c9b7725cd5244878237423f1382ccd4393
Yangfan Zhang <yangfanzhuhai@gmail.com>
2015-06-27 15:28:05 -0700

    added min, max, average query

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 import requests
 import os
 import json
+from sqlalchemy.sql import func
 
 from flask import Flask, Response, request, session, g, redirect, url_for, abort, render_template, flash
+from flask import jsonify
 from urlparse import urljoin
 from flask.ext.sqlalchemy import SQLAlchemy
 
@@ -14,13 +16,16 @@ db = SQLAlchemy(app)
 
 class Shower(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), index=True, unique=True)
-    email = db.Column(db.String(120), index=True, unique=True)
+    name = db.Column(db.String(64), index=True)
+    email = db.Column(db.String(120), index=True)
     shower_length = db.Column(db.Integer, index=True)
     time = db.Column(db.DateTime, index=True, server_default=db.func.now())
 
     def __repr__(self):
-        return '<User %r>' % (self.name)
+        return str(self.__dict__)
+
+    def get_min(self, name):
+        return
 
 
 @app.route('/')
@@ -36,33 +41,13 @@ def account():
 def about():
     return render_template('about.html')
 
-
-@app.route('/mysage')
-def my_sage():
-    return render_template('my_sage.html')
-
-
-@app.route('/api/<path:path>', methods=['GET'])
-def api(path):
-    clientToken = ''
-    clientSecret = ''
-    baseUrl = 'https://suggestqueries.google.com'
-    accessToken = ''
-
-    # Authentication
-    #
-    # session.auth = EdgeGridAuth (
-    #     client_token = clientToken,
-    #     client_secret = clientSecret,
-    #     access_token = accessToken
-    # )
-
-    r = requests.get(urljoin(baseUrl, path), params=request.args)
-
-    if r.status_code != requests.codes.ok:
-        return None
-
-    return Response(r.content,  mimetype='application/json')
+@app.route('/mysage/<username>', methods=['GET'])
+def show_showers(username):
+    shower_min = Shower.query.filter_by(name=username).order_by(Shower.shower_length).first().shower_length
+    shower_max = Shower.query.filter_by(name=username).order_by(Shower.shower_length.desc()).first().shower_length
+    average = db.session.query(func.avg(Shower.shower_length).label('average')).filter(Shower.name==username).first()[0]
+    return render_template('my_sage.html', min=shower_min, max=shower_max,
+                           average=average, name=username)
 
 if __name__ == "__main__":
     app.run(debug=True)
